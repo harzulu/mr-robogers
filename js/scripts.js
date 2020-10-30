7014d10c30be2cd22774e8ada6e0efa05cfd8e92
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-30 09:22:12 -0700

    Add function to build array from 0 to user number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
+function buildArray(number) {
+  let arr = [];
+  for (let i = 0; i <= number; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
 
+$(document).ready(function() {
+  $("form#inputNumber").submit(function(event) {
+    event.preventDefault();
+    let number = parseInt($("#numberInput").val());
+
+  });
 });
