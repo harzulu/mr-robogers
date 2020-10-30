d2a8e29e83fa0b197e00601effe9eff77a385aaf
Benjamin McFarland <benrmcfarland@gmail.com>
2020-10-30 09:49:51 -0700

    Add function to start adding beep boop and neighbor strings

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ function buildArray(number) {
   for (let i = 0; i <= number; i++) {
     arr.push(i);
   }
+  return addBeep(arr);
+}
+
+function addBeep(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      arr[i] = "beep!";
+    }
+  }
+
   return arr;
 }
 
@@ -10,6 +20,6 @@ $(document).ready(function() {
   $("form#inputNumber").submit(function(event) {
     event.preventDefault();
     let number = parseInt($("#numberInput").val());
-
+    console.log(buildArray(number));
   });
 });
